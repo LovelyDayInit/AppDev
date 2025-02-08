7353811126ff135904afd016379f6194500e234c
LovelyDayInit <152673976+LovelyDayInit@users.noreply.github.com>
2025-02-08 11:47:43 +0800

    Styling

--- ProjectOne/src/screens/FirstScreen.js
+++ ProjectOne/src/screens/FirstScreen.js
@@ -3,9 +3,15 @@ import { View, Text, Button } from 'react-native';
 
 const FirstScreen = () => {
     return (
-        <View>
-            <Text>
-                Hello World!
+        <View style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center'
+        }}>
+            <Text style={{ color: 'white', margin: 16 }}>
+                Hello
+                <Text style={{ color: 'red' }}> World</Text>
+                !
             </Text>
 
             <Button title="This is button" />
