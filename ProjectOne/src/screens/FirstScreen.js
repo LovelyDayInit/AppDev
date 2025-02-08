1d2d77bb82d7e6a1111cdd849c84c8d4b50156ce
LovelyDayInit <152673976+LovelyDayInit@users.noreply.github.com>
2025-02-08 11:40:37 +0800

    Add Button

--- ProjectOne/src/screens/FirstScreen.js
+++ ProjectOne/src/screens/FirstScreen.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 const FirstScreen = () => {
     return (
         <View>
             <Text>
-                // This is an example of an Element written in a Javascript Function
                 Hello World!
             </Text>
+
+            <Button title="This is button" />
         </View>
     )
 };
