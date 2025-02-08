33c5e95d49b69ccbfb6360a7b898f81cfac0695f
LovelyDayInit <152673976+LovelyDayInit@users.noreply.github.com>
2025-02-08 11:23:38 +0800

    Add First Screen

---
+++ ProjectOne/src/screens/FirstScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const FirstScreen = () => {
+    return (
+        <View>
+            <Text>
+                // This is an example of an Element written in a Javascript Function
+                Hello World!
+            </Text>
+        </View>
+    )
+};
+
+export default FirstScreen;
