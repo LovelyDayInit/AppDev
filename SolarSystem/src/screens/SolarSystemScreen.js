ec05d3c5b074d5e82dcc7333a2d271449145f669
LovelyDayInit <152673976+LovelyDayInit@users.noreply.github.com>
2025-04-26 12:18:29 +0800

    Images

---
+++ SolarSystem/src/screens/SolarSystemScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text, ImageBackground, ScrollView, TouchableOpacity } from 'react-native';
+const SolarSystemScreen = () => {
+    return (
+        <View style={{ padding: 16, allignItems: 'center'}}> 
+            <Text>SOLAR SYSTEM</Text>
+        </View>
+    )
+};
+
+export default SolarSystemScreen;
